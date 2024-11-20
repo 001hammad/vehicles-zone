d41a6a18f5ea3d1a5ec0ae1cd4edc1463180d224
001hammad <167647584+001hammad@users.noreply.github.com>
2024-11-21 00:10:42 +0500

    Update page.tsx

--- app/(Routes)/Services/imp/exp/page/page.tsx
+++ app/(Routes)/Services/imp/exp/page/page.tsx
@@ -21,7 +21,7 @@ const OurImport = () => {
       />
 
       <p className="text-lg mb-4">
-        The vehicles we import from Japan are thoroughly inspected and reconditioned to meet the highest standards. This ensures that our customers receive vehicles that are not only durable but also efficient and stylish. Whether you're looking for a luxurious car, a reliable bike, or a sturdy bus, we have you covered.
+        The vehicles we import from Japan are thoroughly inspected and reconditioned to meet the highest standards. This ensures that our customers receive vehicles that are not only durable but also efficient and stylish. Whether you&apos;sre looking for a luxurious car, a reliable bike, or a sturdy bus, we have you covered.
       </p>
 
       <p className="text-lg mb-4">
@@ -29,7 +29,7 @@ const OurImport = () => {
       </p>
 
       <p className="text-lg mb-4">
-        If youre interested in any of these imported vehicles, feel free to explore our <a href="/cars" className="text-blue-500 hover:underline">car collection</a>, <a href="/bikes" className="text-blue-500 hover:underline">bike models</a>, or <a href="/buses" className="text-blue-500 hover:underline">buses</a> for more details. We’re here to help you find the perfect vehicle to meet your needs.
+        If youre interested in any of these imported vehicles, feel free to explore our <a href="/cars" className="text-blue-500 hover:underline">car collection</a>, <a href="/bikes" className="text-blue-500 hover:underline">bike models</a>, or <a href="/buses" className="text-blue-500 hover:underline">buses</a> for more details. Were here to help you find the perfect vehicle to meet your needs.
       </p>
 
       <Image
